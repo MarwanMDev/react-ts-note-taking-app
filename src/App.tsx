195cf8a38c3429d71aeca5feaa159db997ba3466
Marwan Mostafa <Marwan.morsi@outlook.com>
2022-11-21 23:34:18 +0200

    Note list

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import NewNote from './components/NewNote';
 import { useLocalStorage } from './customHooks/useLocalStorage';
 import { v4 as uuidV4 } from 'uuid';
+import NoteList from './components/NoteList';
 
 export type Note = {
   id: string;
@@ -60,7 +61,7 @@ function App() {
   return (
     <Container className="my-4">
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<NoteList availableTags={tags} />} />
         <Route
           path="/new"
           element={
