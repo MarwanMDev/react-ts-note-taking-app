e3049d1ce1844b6c060550db86b1a71cb482981d
Marwan Mostafa <Marwan.morsi@outlook.com>
2022-11-22 08:52:15 +0200

    Edit Note Component

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -9,16 +9,19 @@ type NoteFormProps = {
   onSubmit: (data: NoteData) => void;
   onAddTag: (tag: Tag) => void;
   availableTags: Tag[];
-};
+} & Partial<NoteData>;
 
 export default function NoteForm({
   onSubmit,
   onAddTag,
   availableTags,
+  title = '',
+  markdown = '',
+  tags = [],
 }: NoteFormProps) {
   const titleRef = useRef<HTMLInputElement>(null);
   const markdownRef = useRef<HTMLTextAreaElement>(null);
-  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+  const [selectedTags, setSelectedTags] = useState<Tag[]>(tags);
   const navigate = useNavigate();
 
   function handleSubmit(e: FormEvent) {
@@ -40,7 +43,11 @@ export default function NoteForm({
           <Col>
             <Form.Group controlId="title">
               <Form.Label>Title</Form.Label>
-              <Form.Control ref={titleRef} required />
+              <Form.Control
+                ref={titleRef}
+                required
+                defaultValue={title}
+              />
             </Form.Group>
           </Col>
           <Col>
@@ -77,6 +84,7 @@ export default function NoteForm({
             required
             as="textarea"
             rows={15}
+            defaultValue={markdown}
           />
         </Form.Group>
         <Stack
